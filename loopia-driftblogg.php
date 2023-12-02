9a583c763969483cd606174c9892b093f5845f46
Jamie Blomerus <jamie.blomerus@protonmail.com>
2023-12-02 15:06:03 +0100

    WordPress Coding Standards

--- loopia-driftblogg.php
+++ loopia-driftblogg.php
@@ -42,46 +42,47 @@ class Loopia_Driftblogg {
 	 * @return void
 	 */
 	public function loopia_driftblogg_widget_content(): void {
-		// Försök hämta loopia_driftblogg_posts från cache
-		$posts = get_transient('loopia_driftblogg_posts');
+		// Försök hämta loopia_driftblogg_posts från cache.
+		$posts = get_transient( 'loopia_driftblogg_posts' );
 
-		if (false === $posts) {
-			// Hämta inlägg från driftbloggen
-			$url = 'https://driftbloggen.se/wp-json/wp/v2/posts?categories=1&per_page=3';
-			$response = wp_remote_get($url);
+		if ( false === $posts ) {
+			// Hämta inlägg från driftbloggen.
+			$url      = 'https://driftbloggen.se/wp-json/wp/v2/posts?categories=1&per_page=3';
+			$response = wp_remote_get( $url );
 
-			if (is_wp_error($response)) {
+			if ( is_wp_error( $response ) ) {
 				echo 'Error fetching posts';
 				return;
 			}
 
-			$posts = json_decode(wp_remote_retrieve_body($response));
+			$posts = json_decode( wp_remote_retrieve_body( $response ) );
 
-			// Spara inlägg i cache
-			set_transient('loopia_driftblogg_posts', $posts, 300);
+			// Spara inlägg i cache.
+			set_transient( 'loopia_driftblogg_posts', $posts, 300 );
 		}
 
-		if (empty($posts)) {
+		if ( empty( $posts ) ) {
 			echo 'Inga driftstörningar hittades.';
 			return;
 		}
 		?>
 		<ul>
-			<?php foreach ( $posts as $post ) : 
+			<?php
+			foreach ( $posts as $post ) :
 				$post_content = wp_remote_get( $post->link );
 				$post_content = wp_remote_retrieve_body( $post_content );
 
 				$title = wp_trim_words( $post->title->rendered, 8, '...' );
 
-				// Kapa innehållet till det innan <div class="wrapper" id="single-wrapper">
+				// Kapa innehållet till det innan <div class="wrapper" id="single-wrapper">.
 				$post_content = substr( $post_content, 0, strpos( $post_content, '<div class="wrapper" id="single-wrapper">' ) );
 
-				// Hämta flagga
-				$flag = strpos( $post_content, 'flag-ongoing' ) ? 'flag-ongoing' : (strpos( $post_content, 'flag-planned' ) ? 'flag-planned' : 'flag-done');
+				// Hämta flagga.
+				$flag = strpos( $post_content, 'flag-ongoing' ) ? 'flag-ongoing' : ( strpos( $post_content, 'flag-planned' ) ? 'flag-planned' : 'flag-done' );
 				?>
 				<li>
 					<a href="<?php echo esc_attr( $post->link ); ?>" target="_blank">
-						<p><strong><?php echo esc_html($title) ?></strong><span class="flag <?php echo esc_attr( $flag ); ?>"><?php echo $flag == 'flag-ongoing' ? 'Pågående' : ( $flag == 'flag-planned' ? 'Planerad' : 'Avklarad' ); ?></span></p>
+						<p><strong><?php echo esc_html( $title ); ?></strong><span class="flag <?php echo esc_attr( $flag ); ?>"><?php echo 'flag-ongoing' === $flag ? 'Pågående' : ( 'flag-planned' === $flag ? 'Planerad' : 'Avklarad' ); ?></span></p>
 						<p><i>
 						<?php
 						$excerpt = wp_trim_words( $post->excerpt->rendered, 10, '...' );
